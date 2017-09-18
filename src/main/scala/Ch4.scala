04f0c54fb2dd547071454bd67c18c7773f33c296
Radford Nguyen <radford.nguyen@gmail.com>
2017-09-18 17:40:11 -0500

    add 4.5-8

--- src/main/scala/Ch4.scala
+++ src/main/scala/Ch4.scala
@@ -26,10 +26,45 @@ sealed trait Option[+A] {
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
-object Ch4 {
+sealed trait Either[+E,+A] {
+  // ex 4.6
+  def map[B](f: A=>B): Either[E,B] = {
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => Right(f(a))
+    }
+  }
+  def flatMap[EE>:E,B](f: A=>Either[EE,B]): Either[EE,B] = {
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+  }
+  def orElse[EE>:E,B>:A](b: =>Either[EE,B]): Either[EE,B] = {
+    this match {
+      case Left(_) => b
+      case _ => this
+    }
+  }
+  def map2[EE>:E,B,C](b: Either[EE,B])(f: (A,B)=>C): Either[EE,C] = {
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => b.map(b => f(a,b))
+    }
+  }
+}
+case class Left[+E](e:E) extends Either[E,Nothing]
+case class Right[+A](a:A) extends Either[Nothing,A]
+
+object Option {
   def mean(xs: Seq[Double]): Option[Double] = {
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+  }
+
+  def Try[A](a: =>A): Option[A] = {
+    try Some(a)
+    catch { case e: Exception => None }
   }
 
   // ex 4.2
@@ -40,27 +75,30 @@ object Ch4 {
     })
   }
 
-  def Try[A](a: =>A): Option[A] = {
-    try Some(a)
-    catch { case e: Exception => None }
-  }
-
   // ex 4.3
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B)=>C): Option[C] = {
-    a.flatMap(a => {
-      b.map(b => f(a,b))
+    a.flatMap(aa => {
+      b.map(bb => f(aa,bb))
     })
+    // above using flatmaps
+    // below using for-comprehension *syntactic sugar*
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa,bb)
   }
 
   // ex 4.4
   def sequence[A](as: List[Option[A]]): Option[List[A]] = {
-    as.foldRight(Some(Nil): Option[List[A]])((a, acc) => {
-      (a,acc) match {
-        case (None, _) => None
-        case (_, None) => None
-        case (Some(x), Some(xs)) => Some(x::xs)
-      }
-    })
+//    as.foldRight(Some(Nil): Option[List[A]])((a, acc) => {
+//      (a,acc) match {
+//        case (None, _) => None
+//        case (_, None) => None
+//        case (Some(x), Some(xs)) => Some(x::xs)
+//      }
+//    })
+    // reimplemented in terms of traverse
+    traverse(as)(a => a)
   }
 
   // ex 4.5
@@ -73,7 +111,50 @@ object Ch4 {
       }
     })
   }
-  def sequenceT[A](as: List[Option[A]]): Option[List[A]] = {
-    traverse(as)(a => a)
+}
+
+
+object Either {
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] = {
+    if (xs.isEmpty) Left("empty list!")
+    else Right(xs.sum / xs.length)
   }
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] = {
+    try Right(x / y)
+    catch { case e:Exception => Left(e) }
+  }
+
+  def Try[A](a: => A): Either[Exception,A] = {
+    try Right(a)
+    catch { case e:Exception => Left(e) }
+  }
+
+  // ex 4.7
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = {
+    es.foldRight(Right(Nil):Either[E,List[A]])((e, acc) => {
+      (e,acc) match {
+        case (Left(x),_) => Left(x) // this must be first so we always return the first error (since this is foldR)
+        case (_,Left(x)) => Left(x)
+        case (Right(x), Right(xs)) => Right(x::xs)
+      }
+    })
+  }
+  def traverse[E,A,B](as: List[A])(f: A=>Either[E,B]): Either[E,List[B]] = {
+    as.foldRight(Right(Nil):Either[E,List[B]])((a,acc) => {
+      (f(a),acc) match {
+        case (Left(e),_) => Left(e)
+        case (_,Left(e)) => Left(e)
+        case (Right(b),Right(bs)) => Right(b::bs)
+      }
+    })
+  }
+
+  // ex 4.8: i would change map2 to accumulate the error strings (by concat'ing them)
+  //
+  // this could be generalized by having the concat function be customizable, which
+  // would necessitate the introduction of a more-general contain type (than `Either`)
+  //
+  // alternatively, we could use an `Either[List[String], A]`
 }
--- src/test/scala/Ch4Spec.scala
+++ src/test/scala/Ch4Spec.scala
@@ -16,32 +16,42 @@ class Ch4Spec extends FlatSpec with Matchers {
     None.flatMap(root) should be(None)
   }
 
-  "map2" should "just work" in {
+  "Option.map2" should "just work" in {
     val f: (Int, String) => Int = (i,s) => i + Integer.parseInt(s)
-    Ch4.map2(Some(4), Some("3"))(f) should be(Some(7))
-    Ch4.map2(Some(4), None)(f) should be(None)
-    Ch4.map2(None, Some("3"))(f) should be(None)
-    Ch4.map2(None, None)(f) should be(None)
+    Option.map2(Some(4), Some("3"))(f) should be(Some(7))
+    Option.map2(Some(4), None)(f) should be(None)
+    Option.map2(None, Some("3"))(f) should be(None)
+    Option.map2(None, None)(f) should be(None)
   }
 
-  "sequence" should "just work" in {
+  "Option.sequence" should "just work" in {
     val xs = Some(1)::Some(2)::None::Some(4)::Nil
     val ys = Some(1)::Some(2)::Some(3)::Some(4)::Nil
-    Ch4.sequence(xs) should be(None)
-    Ch4.sequence(ys) should be(Some(1::2::3::4::Nil))
+    Option.sequence(xs) should be(None)
+    Option.sequence(ys) should be(Some(1::2::3::4::Nil))
   }
 
-  "traverse" should "just work" in {
-    val f: String => Option[Int] = i => Ch4.Try(i.toInt)
+  "Option.traverse" should "just work" in {
+    val f: String => Option[Int] = i => Option.Try(i.toInt)
 
-    Ch4.traverse("1"::"2"::"3"::"4"::Nil)(f) should be(Some(1::2::3::4::Nil))
-    Ch4.traverse("1"::"2"::"blorp"::"4"::Nil)(f) should be(None)
+    Option.traverse("1"::"2"::"3"::"4"::Nil)(f) should be(Some(1::2::3::4::Nil))
+    Option.traverse("1"::"2"::"blorp"::"4"::Nil)(f) should be(None)
   }
 
-  "sequenceT" should "work same as sequence" in {
-    val xs = Some(1)::Some(2)::None::Some(4)::Nil
-    val ys = Some(1)::Some(2)::Some(3)::Some(4)::Nil
-    Ch4.sequenceT(xs) should be(None)
-    Ch4.sequenceT(ys) should be(Some(1::2::3::4::Nil))
+  "Either.sequence" should "return the first error when multiple errors are present" in {
+    val es = Right(1)::Left("null")::Left("foobar")::Left("empty")::Nil
+    Either.sequence(es) should be(Left("null"))
   }
+
+  "Either.traverse" should "just work" in {
+    val f: String => Either[String, Int] = s => {
+      try Right(s.toInt)
+      catch { case _:Exception => Left(s) }
+    }
+
+    Either.traverse("1"::"2"::"3"::"4"::Nil)(f) should be(Right(1::2::3::4::Nil))
+    Either.traverse("1"::"2"::"blorp"::"4"::Nil)(f) should be(Left("blorp"))
+    Either.traverse("1"::"blep"::"blorp"::"4"::Nil)(f) should be(Left("blep"))
+  }
+
 }
