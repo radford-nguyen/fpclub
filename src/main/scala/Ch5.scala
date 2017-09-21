cfe317c5d79f06168b6b95943ce72d49d5e453c1
Radford Nguyen <radford.nguyen@gmail.com>
2017-09-20 20:15:44 -0500

    add solutions to 5.1-2

---
+++ src/main/scala/Ch5.scala
@@ -0,0 +1,52 @@
+package fpclub5
+
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, _) => Some(h()) // evaluate head (but not tail)
+  }
+  // ex 5.1
+  def toList: List[A] = {
+    this match {
+      case Empty => Nil
+      case Cons(h, t) => h()::t().toList
+    }
+  }
+  // ex 5.2
+  def take(n: Int): Stream[A] = {
+    this match {
+      case Empty => Empty
+      case _ if n <=0 => Empty
+      case Cons(h, t) => Stream.cons(h(), t().take(n-1))
+    }
+  }
+  @annotation.tailrec
+  final def drop(n :Int): Stream[A] = {
+    (n,this) match {
+      case (_,Empty) => Empty
+      case (0,_) => this
+      case (n,Cons(_, t)) => t().drop(n-1)
+    }
+  }
+  // ex 5.3
+  def takeWhile(p: A=>Boolean): Stream[A] = {
+    ???
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: ()=>A, t: ()=>Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](h: =>A, t: =>Stream[A]): Stream[A] = {
+    lazy val head = h
+    lazy val tail = t
+    Cons(()=>head, ()=>tail)
+  }
+  def empty[A](): Stream[A] = Empty
+  def apply[A](as: A*): Stream[A] = {
+    if (as.isEmpty)
+      empty()
+    else
+      cons(as.head, apply(as.tail: _*))
+  }
+}
---
+++ src/test/scala/Ch5Spec.scala
@@ -0,0 +1,40 @@
+package fpclub5
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Ch5Spec extends FlatSpec with Matchers {
+
+  "Stream.take" should "work for n < size of Stream" in {
+    Stream(1,2,3).take(2).toList should be(List(1,2))
+    Stream(1,2,3,4,5,6,7).take(4).toList should be(List(1,2,3,4))
+  }
+  "Stream.take" should "return Stream for n == size of Stream" in {
+    val s_4 = Stream(1,2,3,4)
+    val s_14 = Stream(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+    s_4.take(4).toList should be(s_4.toList)
+    s_14.take(14).toList should be(s_14.toList)
+  }
+  "Stream.take" should "return Stream for n > size of Stream" in {
+    Stream(1,2,3).take(22).toList should be(List(1,2,3))
+    Stream(1,2,3).take(12).toList should be(List(1,2,3))
+  }
+  "Stream.take" should "return Empty on empty Stream" in {
+    Stream.empty.take(8) should be(Empty)
+  }
+  "Stream.take" should "return Empty for n <= 0" in {
+    val s = Stream(1,2,3,4)
+    s.take(0) should be(Empty)
+    s.take(-2) should be(Empty)
+    s.take(-9) should be(Empty)
+  }
+
+  "Stream.drop" should "work for 0 < n < size of Stream" in {
+    Stream(1,2,3).drop(1).toList should be(List(2,3))
+    Stream(1,2,3,4,5,6,7).drop(4).toList should be(List(5,6,7))
+  }
+  "Stream.drop" should "return Empty for n > size of Stream" in {
+    Stream(1,2,3).drop(23) should be(Empty)
+    Stream(1,3).drop(23) should be(Empty)
+    Stream(1,2,3,4,5,6,7,8,9,10,11,12,13).drop(23) should be(Empty)
+  }
+}
