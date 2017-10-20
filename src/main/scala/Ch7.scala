8e1c34cb7fea15a5367e83ec3cf30f053b7c6109
Radford Nguyen <radford.nguyen@amd.com>
2017-10-20 18:24:47 -0500

    add thru 7.14

--- src/main/scala/Ch7.scala
+++ src/main/scala/Ch7.scala
@@ -2,6 +2,8 @@ package fpclub7
 
 import java.util.concurrent
 
+import fpclub7.Ch7.{Callable, ExecutorService}
+
 import scala.concurrent.duration.TimeUnit
 
 object Ch7 {
@@ -164,6 +166,49 @@ object Ch7 {
       * ```
       */
 
+
+    // ex 7.11
+    def choiceN[A](n: Par[Int])(choices: List[Par[A]]): Par[A] = es => {
+      choices.drop(run(es)(n).get).head(es)
+    }
+    def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] = {
+      choiceN(Par.map(cond)(if (_) 0 else 1))(
+        List(t,f)
+      )
+    }
+
+    // ex 7.13
+    def chooser[A,B](p: Par[A])(choices: A=>Par[B]): Par[B] = es => {
+      choices(run(es)(p))(es)
+    }
+    def choiceN_asChooser[A](n: Par[Int])(choices: List[Par[A]]): Par[A] = {
+      chooser(n)(choices.drop(_).head)
+    }
+    def choice_asChooser[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] = {
+      chooser(cond)(if (_) t else f)
+    }
+    // `chooser` is really too specific a name as it is now
+    // more general than the act of making a choice.  we
+    // call it `flatmap` (or `bind`)
+    def flatMap[A,B](p: Par[A])(f: A=>Par[B]): Par[B] = es => {
+      f(run(es)(p))(es)
+    }
+
+    // ex 7.14
+    /**
+      * Returns a Par that waits for an inner parallel computation to complete
+      * before returning the result
+      *
+      * @param a
+      * @tparam A
+      * @return
+      */
+    def join[A](a: Par[Par[A]]): Par[A] = es => {
+      map(a)(run(es)(_).get)(es)
+    }
+
+
+
   }
 
   /* Gives us infix syntax for `Par`. */
@@ -203,3 +248,103 @@ object Ch7 {
     override def isCancelled: Boolean = false
   }
 }
+
+//object Actors {
+//  sealed trait Future[A] {
+//    private[fpclub7] def apply(k: A => Unit): Unit
+//  }
+//  type Par[+A] = ExecutorService => Future[A]
+//
+//  object Par {
+//    def unit[A](a: A): Par[A] = _ => new Future[A] {
+//      def apply(k: A => Unit): Unit = k(a)
+//    }
+//
+//    def fork[A](a: => Par[A]): Par[A] = exec => {
+//      // evaluate an action asynchronously using an ExecutorService
+//      def eval(e: ExecutorService)(r: => Unit): Unit = {
+//        e.submit(new Callable[Unit] { def call = r })
+//      }
+//      new Future[A] {
+//        override private[fpclub7] def apply(k: (A) => Unit): Unit = {
+//          eval(exec)(a(exec)(k))
+//        }
+//      }
+//    }
+//
+//    def lazyUnit[A](a: => A) = fork(unit(a))
+//
+//    // ex 7.1
+//    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = exec => {
+//      new Future[C] {
+//        override private[fpclub7] def apply(k: (C) => Unit): Unit = {
+//          var ar: Option[A] = None
+//          var br: Option[B] = None
+//          val combiner = Actor[Either[A,B]](exec) {
+//
+//          }
+//        }
+//      }
+//    }
+//
+//    /**
+//      * Runs the given `Par`, spawning parallel computations as
+//      * marked, and returns the `Future` result.
+//      *
+//      * @param a
+//      * @tparam A
+//      * @return
+//      */
+//    def run[A](s: ExecutorService)(a: Par[A]): Future[A] = {
+//      a(s)
+//    }
+//
+//    // ex 7.4
+//    def asyncF[A, B](f: A => B): A => Par[B] = {
+//      a => lazyUnit(f(a))
+//    }
+//
+//    def map[A, B](pa: Par[A])(f: A => B): Par[B] = {
+//      lift(f)(pa)
+//    }
+//
+//    def lift[A, B](f: A => B): Par[A] => Par[B] = {
+//      pa => map2(pa, unit(()))((a, _) => f(a))
+//    }
+//
+//    /**
+//      * Map a function `f` over a list in _parallel_
+//      *
+//      * @param ps
+//      * @param f
+//      * @tparam A
+//      * @tparam B
+//      * @return
+//      */
+//    def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork({
+//      val fbs: List[Par[B]] = ps.map(asyncF(f))
+//      sequence(fbs)
+//    })
+//
+//    // ex 7.5
+//    def sequence[A](ps: List[Par[A]]): Par[List[A]] = {
+//      def init: Par[List[A]] = unit(Nil: List[A])
+//
+//      ps.foldRight(init)((pa, parList) => {
+//        map2(pa, parList)((a, as) => a :: as)
+//      })
+//    }
+//
+//    // ex 7.6
+//    def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+//      val af: A => Par[Option[A]] = asyncF(a => if (f(a)) Some(a) else None)
+//      val ps: List[Par[Option[A]]] = as.map(af)
+//      ps.foldRight(unit(Nil: List[A]))((pa, parList) => {
+//        map2(pa, parList)((a, as) => a match {
+//          case Some(_a) => _a :: as
+//          case _ => as
+//        })
+//      })
+//    }
+//  }
+//}
